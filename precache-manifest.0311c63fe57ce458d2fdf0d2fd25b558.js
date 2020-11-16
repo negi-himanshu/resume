b88371d6c75b660b21a419923f421cd93bca0df5
Himanshu Negi <himanshunegi.53@gmail.com>
2020-11-17 01:51:59 +0530

    Updates

---
+++ precache-manifest.0311c63fe57ce458d2fdf0d2fd25b558.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f49e6e41ca1930cc778820794f114f7b",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "944032491096155433d0",
+    "url": "/resume/static/css/main.06d07d6a.chunk.css"
+  },
+  {
+    "revision": "a88dffa7c0b8dc97b9d5",
+    "url": "/resume/static/js/2.3eb01123.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/resume/static/js/2.3eb01123.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "944032491096155433d0",
+    "url": "/resume/static/js/main.3025b8f3.chunk.js"
+  },
+  {
+    "revision": "af320e8d8166d7788a55",
+    "url": "/resume/static/js/runtime-main.4635711e.js"
+  },
+  {
+    "revision": "f84845bc6d350f1df68317649e9ef245",
+    "url": "/resume/static/media/calendar.f84845bc.svg"
+  },
+  {
+    "revision": "50932b68b38f92f814dee16d979895f7",
+    "url": "/resume/static/media/football.50932b68.svg"
+  },
+  {
+    "revision": "cf9e1cd4de25ed44c579fe8b8c767355",
+    "url": "/resume/static/media/location.cf9e1cd4.svg"
+  }
+]);
